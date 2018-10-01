530b58152a8117f649fe2c15d4ac02fb52129b3b
nmiguenz <37988654+nmiguenz@users.noreply.github.com>
2018-10-01 00:06:00 -0300

    Proyecto terminado y funcionando

---
+++ TP-02/Entidades/Changuito.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    /// <summary>
+    /// No podrá tener clases heredadas.
+    /// </summary>
+    public class Changuito
+    {
+        List<Producto> productos;
+        int espacioDisponible;
+        public enum ETipo
+        {
+            Dulce, Leche, Snacks, Todos
+        }
+
+        #region "Constructores"
+        private Changuito()
+        {
+            this.productos = new List<Producto>();
+        }
+
+        // Reutilización de código
+        public Changuito(int espacioDisponible) : this()
+        {
+            this.espacioDisponible = espacioDisponible;
+        }
+        #endregion
+
+        #region "Sobrecargas"
+        /// <summary>
+        /// Muestro el Changuito y TODOS los Productos
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Mostrar(this, ETipo.Todos);
+        }
+        #endregion
+
+        #region "Métodos"
+
+        /// <summary>
+        /// Expone los datos del elemento y su lista (incluidas sus herencias)
+        /// SOLO del tipo requerido
+        /// </summary>
+        /// <param name="c">Elemento a exponer</param>
+        /// <param name="ETipo">Tipos de ítems de la lista a mostrar</param>
+        /// <returns></returns>
+        public string Mostrar(Changuito c, ETipo tipo)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Tenemos {0} lugares ocupados de un total de {1} disponibles", c.productos.Count, c.espacioDisponible);
+            sb.AppendLine("");
+            foreach (Producto producto in c.productos)
+            {
+                switch (tipo)
+                {
+                    case ETipo.Snacks:
+                        if (producto is Snacks)
+                        {
+                            sb.AppendLine(producto.Mostrar());
+                        }
+                        break;
+                    case ETipo.Dulce:
+                        if (producto is Dulce)
+                        {
+                            sb.AppendLine(producto.Mostrar());
+                        }
+                        break;
+                    case ETipo.Leche:
+                        if (producto is Leche)
+                        {
+                            sb.AppendLine(producto.Mostrar());
+                        }
+                        break;
+                    default:
+                        sb.AppendLine(producto.Mostrar());
+                        break;
+                }
+            }
+            return sb.ToString();
+        }
+        #endregion
+
+        #region "Operadores"
+        /// <summary>
+        /// Agregará un elemento a la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se agregará el elemento</param>
+        /// <param name="p">Objeto a agregar</param>
+        /// <returns></returns>
+        public static Changuito operator +(Changuito c, Producto p)
+        {
+            foreach (Producto producto in c.productos)
+            {
+                if (producto == p)
+                    return c;
+            }
+
+            c.productos.Add(p);
+            return c;
+        }
+        /// <summary>
+        /// Quitará un elemento de la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se quitará el elemento</param>
+        /// <param name="p">Objeto a quitar</param>
+        /// <returns></returns>
+        public static Changuito operator -(Changuito c, Producto p)
+        {
+            foreach (Producto producto in c.productos)
+            {
+                if (producto == p)
+                {
+                    break;
+                }
+            }
+            c.productos.Remove(p);
+            return c;
+        }
+        #endregion
+    }
+}
---
+++ TP-02/Entidades/Producto.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public enum EMarca
+    {
+        Serenisima, Campagnola, Arcor, Ilolay, Sancor, Pepsico
+    }
+    /// <summary>
+    /// La clase Producto no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Producto
+    {
+        
+        EMarca marca;
+        string codigoDeBarras;
+        ConsoleColor colorPrimarioEmpaque;
+
+        /// <summary>
+        /// ReadOnly: Retornará la cantidad de calorias del producto
+        /// </summary>
+        protected abstract short CantidadCalorias {
+            get;    
+        }
+
+        /// <summary>
+        /// Publica todos los datos del Producto.
+        /// </summary>
+        /// <returns></returns>
+        public abstract string Mostrar();
+
+        public static explicit operator string(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(string.Format("CODIGO DE BARRAS: {0}\r\n", p.codigoDeBarras));
+            sb.AppendLine(string.Format("MARCA          : {0}\r\n", p.marca.ToString()));
+            sb.AppendLine(string.Format("COLOR EMPAQUE  : {0}\r\n", p.colorPrimarioEmpaque.ToString()));
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos productos son iguales si comparten el mismo código de barras
+        /// </summary>
+        /// <param name="productoUno"></param>
+        /// <param name="productoDos"></param>
+        /// <returns></returns>
+        public static bool operator ==(Producto productoUno, Producto productoDos)
+        {
+            return (productoUno.codigoDeBarras == productoDos.codigoDeBarras);
+        }
+        /// <summary>
+        /// Dos productos son distintos si su código de barras es distinto
+        /// </summary>
+        /// <param name="productoUno"></param>
+        /// <param name="productoDos"></param>
+        /// <returns></returns>
+        public static bool operator !=(Producto productoUno, Producto productoDos)
+        {
+            return !(productoUno == productoDos);
+        }
+
+        public Producto (string patente, EMarca marca, ConsoleColor color)
+        {
+            this.codigoDeBarras = patente;
+            this.marca = marca;
+            this.colorPrimarioEmpaque = color;
+        }
+    }
+}
