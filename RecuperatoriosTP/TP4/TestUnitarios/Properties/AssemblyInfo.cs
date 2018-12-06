2273e6118589bffc23960d063e4c07a85ee341f8
nmiguenz <37988654+nmiguenz@users.noreply.github.com>
2018-12-06 15:48:45 -0300

    TP Terminado
    Incluyo la solución. Sigue sin funcionar la intermitencia del cursor

---
+++ RecuperatoriosTP/TP4/TestUnitarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnitarios")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4eb54cf3-fcec-4200-a695-f9b32a2f16b4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RecuperatoriosTP/TP4/TestUnitarios/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Entidades;
+using System.Collections.Generic;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ListaPaquetesDeCorreo_VerificaInstancia()
+        {
+            List<Paquete> paquetes;
+
+            Correo correo = new Correo();
+
+            paquetes = correo.Paquetes;
+
+            Assert.IsNotNull(paquetes);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TrackingIdRepetidoException))]
+        public void VerificaPaquetesPorID_LanzaTrackingIdRepetidoException()
+        {
+            Correo correo = new Correo();
+            Paquete p1 = new Paquete("9 de Julio", "1234");
+            Paquete p2 = new Paquete("Azcuenaga", "1234");
+
+            correo += p1;
+            correo += p2;
+            Assert.Fail();
+        }
+    }
+}
